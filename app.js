61e282e29dd2b49a577d525e19b40eca04c77c17
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-02-17 14:48:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
-"use strict";
-
 // ========== Variables ========== //
-let number = 0;
 
 // ========== Event Functions ========== //
 
-function incrementNumber() {
-    number = number + 1;
-    displayNumber();
-}
-
-function decrementNumber() {
-    number = number - 1;
-    displayNumber();
-}
-
 // ========== Display Functions ========== //
 
-function displayNumber() {
-    document.querySelector("#number").textContent = number;
-    document.querySelector("#message").textContent = `The number is: ${number}`;
-    // document.querySelector("#message").textContent = "The number is:" + number;
-}
-
 // ========== Event Listeners ========== //
-document.querySelector("#btn-increment").addEventListener("click", incrementNumber);
-document.querySelector("#btn-decrement").addEventListener("click", decrementNumber);
