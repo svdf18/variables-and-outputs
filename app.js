22d8e3c8b28f32602a06d434a831df6b33ac9774
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-02-17 10:36:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ let number = 0;
 function increment() {
     console.log(number);
     number = number + 1;
-    document.querySelector("#number").innerText = number;
-    document.querySelector("#message").innerText =
-        "You incremented the number 🎉";
+    displayNumber();
+    document.querySelector("#message").textContent = "You incremented the number 🎉 The number is now: " + number;
 }
 
 function decrement() {
     console.log(number);
     number = number - 1;
-    document.querySelector("#number").innerText = number;
-    document.querySelector("#message").innerText =
-        "You decremented the number 💁🏼‍♂️";
+    displayNumber();
+    document.querySelector("#message").textContent = `You decremented the number 💁🏼‍♂️ The number is now: ${number}`;
+}
+
+function displayNumber() {
+    document.querySelector("#number").textContent = number;
 }
 
 document.querySelector("#btn-increment").addEventListener("click", increment);
